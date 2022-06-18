24a6923e5df5e09d63e2ef513058e526c87423ab
Ishimwe Jules Pacis (Mediator) <51479761+pacisjules@users.noreply.github.com>
2022-06-19 01:33:51 +0200

    Update Credentials

--- configs/connection.py
+++ configs/connection.py
@@ -9,14 +9,14 @@ def db_config():
     return dbinfo.Setting()
 
 def DATABASE_URL(
-    connection: str = "postgres://qlmasrswywqndr:a015291058aed5a7c55bd51a62b291a6bc8e746969c101d8aff32ad723f680df@ec2-52-73-184-24.compute-1.amazonaws.com:5432/dcv839v8dctfto",
+    connection: str = "postgresql",
     username: str   = "qlmasrswywqndr",
     password: str   ="a015291058aed5a7c55bd51a62b291a6bc8e746969c101d8aff32ad723f680df",
     host: str       = "ec2-52-73-184-24.compute-1.amazonaws.com",
     port: str       = "5432",
-    database: str   = "dcv839v8dctfto"
+    database: str   = "ec2-52-73-184-24.compute-1.amazonaws.com"
 ):
-    return str(connection+"://"+username+":"+password+"@"+host+":"+port+"/"+database)
+    return str(connection+"://qlmasrswywqndr:a015291058aed5a7c55bd51a62b291a6bc8e746969c101d8aff32ad723f680df@ec2-52-73-184-24.compute-1.amazonaws.com:5432/dcv839v8dctfto")
 
 database = databases.Database(DATABASE_URL())
 
